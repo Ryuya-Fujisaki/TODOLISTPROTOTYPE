cb5c98102b95fd4667b9e4e1a8ad381fa7dc5fe0
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2023-07-28 10:56:37 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Udemy視聴React学習' },
+    { id: 2, title: 'ReactTODOリストアイデア思案' },
+    { id: 3, title: '8月プログラミング学習スケジュール確認' }
+  ])
+  const [todoTitle, setTodoTitle] = useState('')
+  const [todoId, setTodoId] = useState(todos.length + 1)
+  const handleAddFormChanges = (e) => { setTodoTitle(e.target.value) }
+  const handleAddTodo = () => {
+    setTodos([...todos, { id: todoId, title: todoTitle }])
+    setTodoId(todoId + 1)
+    setTodoTitle('')
+  }
+
+  const handleDeleteTodo = (targetTodo) => {
+    console.log(targetTodo)
+
+    setTodos(todos.filter((todo) => todo !== targetTodo))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        label="タイトル"
+        value={todoTitle}
+        onChange={handleAddFormChanges}
+      />
+      <button onClick={handleAddTodo}>作成</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            <button onClick={() => handleDeleteTodo(todo)}>削除</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
